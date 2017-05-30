8247f47bb0ed3c3bfb12f05313fc604f034c5e05
Anwesh43 <anweshthecool0@gmail.com>
2017-05-30 17:59:16 +0530

    drawing text in collapsible button

--- collapsible-text-component.js
+++ collapsible-text-component.js
@@ -1,47 +1,69 @@
-const maxW = window.innerWidth/10,currW = window.innerWidth/20
+const maxW = window.innerWidth/4,currW = window.innerWidth/20
 class CollapsibleTextComponent extends HTMLElement {
     constructor() {
-        this.text = this.getAttribute('text')
+        super()
+        this.text = this.getAttribute('text')||"dummytext"
         const shadow = this.attachShadow({mode:'open'})
         this.img = document.createElement('img')
         shadow.appendChild(this.img)
         this.state = {wx:currW,dir:0}
         this.collapsibleButton = new CollapsibleButton()
+        // const x = parseFloat(this.getAttribute('x')||"100"),y = parseFloat(this.getAttribute('y')||"100")
+        // this.img.style.position = 'absolute'
+        // this.img.style.left = x
+        // this.img.style.top = y
     }
     render() {
         const canvas = document.createElement('canvas')
         canvas.width = this.state.wx
-        canvas.height = window.innerHeight/10
+        canvas.height = currW
         const context = canvas.getContext('2d')
-        context.font = context.font.replace(/\d{2}/,30)
+        context.font = context.font.replace(/\d{2}/,currW/2)
         const w = context.measureText(this.text).width
+        if(!this.maxW) {
+            this.maxW = Math.max(w+2*currW+w/5,2*w)
+        }
         context.clearRect(0,0,canvas.width,canvas.height)
         context.fillStyle = 'gray'
         context.fillRect(0,0,canvas.width,canvas.height)
-        collapsibleButton.draw(context,this.wx,this.dir)
-        this.wx += this.dir * currW/5
-        if(this.wx >= maxW) {
-            this.wx = maxW
-            this.dir= 0
+        this.collapsibleButton.draw(context,this.state.wx,this.state.dir)
+        context.fillStyle = 'white'
+        context.fillText(this.text,currW+w/40,currW/2)
+        this.state.wx += this.state.dir * currW/5
+        if(this.state.wx > this.maxW) {
+            this.state.wx = this.maxW
+            this.state.dir= 0
+            //console.log(this.state.dir)
+            this.collapsibleButton.reset(1)
+
         }
-        if(this.wx <= currW) {
-            this.wx = currW
-            this.dir = 0
+        if(this.state.wx < currW) {
+            this.state.wx = currW
+            this.state.dir = 0
+            this.collapsibleButton.reset(-1)
+            //clearInterval(this.interval)
         }
         this.img.src = canvas.toDataURL()
+        //console.log("rendering")
     }
     connectedCallback() {
         this.render()
         this.img.onmousedown = () => {
-          if(this.dir == 0) {
+          if(this.state.dir == 0) {
+            if(this.state.wx <= currW) {
+                this.state.dir = 1
+                //console.log("start +")
+            }
+            else if(this.state.wx >= this.maxW){
+                this.state.dir = -1
+                //console.log("start -")
+            }
               const interval = setInterval(()=>{
-                  if(this.wx <= currW) {
-                      this.dir = 1
+                  if(this.state.dir == 0) {
+                      clearInterval(interval)
                   }
-                  else if(this.wx >= maxW){
-                      this.dir = -1
-                  }
-              },100)
+                  this.render()
+              },20)
            }
        }
     }
@@ -57,7 +79,7 @@ class CollapsibleButton {
         context.lineWidth = r/10
         context.save()
         context.translate(w-r,r)
-        context.rotate(this.rot)
+        context.rotate(this.rot*Math.PI/180)
         context.beginPath()
         context.arc(0,0,r,0,2*Math.PI)
         context.fill()
@@ -73,4 +95,8 @@ class CollapsibleButton {
         context.restore()
         this.rot += 9*dir
     }
+    reset(dir) {
+        this.rot = dir == 1?45:0
+    }
 }
+customElements.define('collapsible-text',CollapsibleTextComponent)
