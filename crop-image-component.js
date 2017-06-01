e166ea6c7280a5077a4f5cf8429684dccba0d840
Anwesh43 <anweshthecool0@gmail.com>
2017-06-01 19:54:48 +0530

    adding crop image component

---
+++ crop-image-component.js
@@ -0,0 +1,49 @@
+class CropImageComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+        this.image = new Image()
+        this.img.src = this.getAttribute('src')
+        this.image.src = this.img.src
+        this.img.draggable = 'false'
+    }
+    render(scale) {
+        const canvas = document.createElement('canvas')
+        const w = this.img.width,h = this.img.height
+        canvas.width = this.img.width
+        canvas.height = this.img.height
+        const context = canvas.getContext('2d')
+        context.beginPath()
+        context.rect(w/2-scale*w/2,h/2-scale*h/2,w*scale,h*(scale))
+        context.clip()
+        context.drawImage(this.image,0,0)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        var isDown = false
+        var points = []
+        var started = false
+        this.img.onmousedown = ()=>{
+            if(!started) {
+            started = true
+            var dir = 1,scale = 1
+            const interval = setInterval(()=>{
+                this.render(scale)
+                scale -= 0.2*dir
+                if(scale < 0) {
+                    dir = -1
+                }
+                if(scale > 1) {
+                    scale  = 1
+                    dir = 0
+                    started = false
+                    clearInterval(interval)
+                }
+            },40)
+          }
+        }
+    }
+}
+customElements.define('crop-image',CropImageComponent)
