15d39a84cfb431f231457dfe643bb5eae5dddf5d
Anwesh43 <anweshthecool0@gmail.com>
2017-06-05 14:19:53 +0530

    adding quarter

--- quarter-image-component.js
+++ quarter-image-component.js
@@ -5,6 +5,7 @@ class QuarterImageComponent extends HTMLElement {
         const shadow = this.attachShadow({mode:'open'})
         shadow.appendChild(this.img)
         this.src = this.getAttribute('src')
+        this.color = this.getAttribute('color') || 'green'
     }
     render() {
         const w = this.image.width , h = this.image.height
@@ -30,3 +31,44 @@ class QuarterImageComponent extends HTMLElement {
     }
 }
 customElements.define('quarter-image',QuarterImageComponent)
+class Quarter  {
+    constructor(i,boundsCb) {
+        this.deg = 90*i
+        this.a = 0
+        this.dir = 0
+        this.boundsCb = boundsCb
+    }
+    draw(context,x,y,r,color)  {
+        context.save()
+        context.fillStyle = color
+        context.globalAlpha = 0.5
+        context.translate(x,y)
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var i=this.deg;i<=this.deg+this.a;i++) {
+            const x = r*Math.cos(i*Math.PI/180), y = r*Math.sin(i*Math.PI/180)
+            context.lineTo(x,y)
+        }
+        context.fill()
+        context.restore()
+    }
+    update() {
+        this.a += 18*this.dir
+        if(this.a > 90) {
+            this.a = 90
+            this.dir = 0
+        }
+        else if(this.a < 0) {
+            this.a = 0
+            this.dir = 0
+        }
+    }
+    handleTap(x,y) {
+        if(this.boundsCb(x,y) == true && this.dir == 0) {
+            this.dir = 1
+            if(this.a == 90) {
+                this.dir = -1
+            }
+        }
+    }
+}
