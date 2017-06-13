96a7da9000188d3ef3e42df3872f12887f937f15
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 13:13:10 +0530

    adding CollapsibleButton

--- collapsible-list-component.js
+++ collapsible-list-component.js
@@ -11,6 +11,7 @@ class CollapsibleListComponent  extends HTMLElement {
               const child = this.children[index]
               this.elements.push(child.innerHTML)
         }
+        this.collapButton = new CollapsibleButton()
     }
     connectedCallback() {
         this.render()
@@ -22,8 +23,49 @@ class CollapsibleListComponent  extends HTMLElement {
         canvas.height = (fontSize*2)*(this.elements.length+1)
         const context = canvas.getContext('2d')
         context.fillStyle = '#E0E0E0'
-        context.fillRect(0,0,canvas.width,canvas.height)
+        context.fillRect(0,0,canvas.width,2*fontSize)
+        this.collapButton.draw(context,0.8*canvas.width,fontSize,fontSize/2)
         this.img.src = canvas.toDataURL()
+    }
+}
+class CollapsibleButton {
+    constructor() {
+        this.rot = 0
+    }
+    draw(context,x,y,r) {
+        if(!this.x && !this.y && !this.r) {
+            this.x = x
+            this.y = y
+            this.r = r
+        }
+        context.lineWidth = r/6
+        context.lineCap = 'round'
+        context.strokeStyle = 'black'
+        context.save()
+        context.translate(x,y)
+        context.rotate(this.rot*Math.PI/180)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.rotate(i*Math.PI/2)
+            context.beginPath()
+            context.moveTo(0,-r)
+            context.lineTo(0,r)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    setEdgeValue(dir) {
+        this.rot = 0
+        if(dir == 1) {
+            this.rot = 90
+        }
+    }
+    update(dir) {
+        this.rot += 18*dir
+    }
+    handleTap(x,y) {
+       return x >= this.x -this.r && x <= this.x + this.r && y>= this.y - this.r && y <= this.y + this.r
     }
 }
 customElements.define('collap-list',CollapsibleListComponent)
