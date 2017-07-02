96f2bcf7c82295a6d5ccde6915a7edfa5c317590
Anwesh43 <anweshthecool0@gmail.com>
2017-07-02 23:09:43 +0530

    adding animationhandler

--- boundary-fill-shape-component.js
+++ boundary-fill-shape-component.js
@@ -13,6 +13,8 @@ class BoundaryFillShapeComponent extends HTMLElement {
         canvas.width = w/10
         canvas.height = w/10
         const context = canvas.getContext('2d')
+        drawShape(context,canvas.width)
+        this.animate(getBoundaryPoints(canvas.width))
         this.img.src = canvas.toDataURL()
     }
     drawShape(context,size) {
@@ -24,7 +26,24 @@ class BoundaryFillShapeComponent extends HTMLElement {
     getBoundaryPoints(size) {
         return []
     }
+    stopped() {
+        return false
+    }
     connectedCallback() {
         this.render()
+    }
+}
+class AnimationHandler {
+    constructor(component) {
+        this.animated = false
+        this.component = component
+    }
+    startAnimating() {
+        if(this.animated == false) {
+            this.animated = true
+            const interval = setInterval(()=>{
+                this.component.render()
+            },50)
+        }
     }
 }
