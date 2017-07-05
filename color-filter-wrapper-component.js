8910425b128f9636d848a11c8804045252e0046f
Anwesh43 <anweshthecool0@gmail.com>
2017-07-05 15:46:53 +0530

    adding ColorFilterImage

--- color-filter-wrapper-component.js
+++ color-filter-wrapper-component.js
@@ -14,10 +14,42 @@ class ColorFilterWrapperComponent extends HTMLElement {
         }
     }
     render() {
-        
+
     }
     connectedCallback() {
         this.render()
+    }
+}
+class ColorFilterImage {
+    constructor(img) {
+        this.img = img
+        this.scale = 0
+        this.dir = 0
+
+    }
+    draw(color) {
+        const canvas =  document.createElement('canvas')
+        canvas.width = this.img.width
+        canvas.height = this.img.height
+        const context = canvas.getContext('2d')
+        context.drawImage(this.img,0,0)
+        context.save()
+        context.translate(canvas.width/2,canvas.height/2)
+        context.fillStyle = color
+        context.globalAlpha = 0.6
+        context.fillRect(-canvas.width/2,-canvas.height/2,canvas.width,canvas.height)
+        context.restore()
+        this.img,src = canvas.toDataURL()
+    }
+    update() {
+        this.scale += 0.2
+        if(this.scale >1) {
+            this.scale = 0
+            this.dir = 0
+        }
+    }
+    stopped() {
+        return this.dir == 0
     }
 }
 customElements.define('color-filter-wrapper',ColorFilterWrapperComponent)
