0810062efba531666f3d9c67ae565b259f2703b1
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 15:07:53 +0530

    adding Bordered Image

--- bordererd-image-component.js
+++ bordererd-image-component.js
@@ -5,6 +5,7 @@ class BorderedImageComponent extends HTMLElement {
         this.img = document.createElement('img')
         shadow.appendChild(img)
         this.src = this.getAttribute('src')
+        this.color =
     }
     render() {
         const canvas = document.createElement('canvas')
@@ -19,5 +20,60 @@ class BorderedImageComponent extends HTMLElement {
         this.image.onload = ()=>{
             this.render()
         }
+    }
+}
+class BorderedImage {
+    constructor(image) {
+        this.image = image
+        this.scale = 0
+        this.dir = 0
+    }
+    draw(context,x,y,color) {
+        const imw = this.image.width,imw = this.image.height
+        context.strokeStyle = color
+        context.lineWidth = Math.min(imw,imh)/50
+        context.drawImage(image,x-imw/2,y-imh/2)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.translate(x,y)
+            context.rotate(i*180)
+            for(var j=0;j<2;j++) {
+                context.save()
+                context.beginPath()
+                context.moveTo(imw/2,0)
+                context.lineTo(imw/2,imh/2*this.scale*j)
+                context.stroke()
+                context.restore()
+                context.save()
+                context.beginPath()
+                context.moveTo(0,imh/2)
+                context.lineTo(imw/2*this.scale*j,imh/2)
+                context.stroke()
+                context.restore()
+            }
+            context.restore()
+        }
+    }
+    update() {
+        this.scale += this.dir*0.2
+        if(this.scale > 1) {
+            this.dir = 0
+            this.scale = 1
+        }
+        if(this.scale < 0) {
+            this.dir = 0
+            this.scale = 1
+        }
+    }
+    startUpdating() {
+        if(this.scale == 0) {
+            this.dir = 1
+        }
+        else {
+            this.dir = -1
+        }
+    }
+    stopped() {
+        return this.dir == 0
     }
 }
