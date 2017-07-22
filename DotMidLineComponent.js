e4f1392ec8691f8796ce1fdb02eb5f219e04a11a
Anwesh43 <anweshthecool0@gmail.com>
2017-07-22 21:31:31 +0530

    adding DotMidLine

--- DotMidLineComponent.js
+++ DotMidLineComponent.js
@@ -10,6 +10,29 @@ class DotMidLineComponent extends HTMLElement {
 
     }
     connectedCallback(){
-
+        this.render()
+    }
+}
+class DotMidLine {
+    draw(context,x,y,size,scale) {
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.translate(x,y)
+            context.scale(2*i-1,1)
+            context.lineWidth = size/60
+            this.drawCircle(4*size/5,0,size/5,1)
+            context.stroke()
+            this.drawCircle(4*size/5,0,size/5,scale)
+            context.fill()
+            context.beginPath()
+            context.moveTo(0,0)
+            context.lineTo((3*size/5)*scale,0)
+            context.stroke()
+            context.restore()
+        }
+    }
+    drawCircle(x,y,r,scale) {
+        context.beginPath()
+        context.arc(x,y,r*scale,0,2*Math.PI)
     }
 }
