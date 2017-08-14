3d2e37c87fa90e30ed48eabaf7eb2a90191ca928
Anwesh43 <anweshthecool0@gmail.com>
2017-08-14 19:30:54 +0530

    adding InelasticRenderer

--- InelasticJumpComponent.js
+++ InelasticJumpComponent.js
@@ -14,11 +14,29 @@ class InelasticJumpComponent extends HTMLElement {
         this.div.style.position = "absolute"
         this.div.style.left = w/2-w/24
         this.div.style.top = h-w/12
+        this.renderer = new InelasticRenderer()
     }
     update() {
-        this.div.style.top = 100
+        this.renderer.update()
+        this.div.style.top = this.renderer.y
     }
     connectedCallback() {
         this.initDiv()
+    }
+}
+class InelasticRenderer {
+    constructor(y,a) {
+        this.initY = y
+        this.y = y
+        this.a = a
+        this.afact = a/10
+        this.deg = 0
+    }
+    update() {
+        this.y = this.initY - this.a*Math.abs(Math.sin(this.deg*Math.PI/180))
+        this.deg += 20
+        if(this.deg %180 == 0) {
+            this.a -= this.afact
+        }
     }
 }
