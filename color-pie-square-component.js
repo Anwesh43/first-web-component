81d0891d2e22e5464cb1a2786824dc9cc8dd1af1
Anwesh43 <anweshthecool0@gmail.com>
2017-08-31 12:58:45 +0530

    adding ColorPie

--- color-pie-square-component.js
+++ color-pie-square-component.js
@@ -17,3 +17,26 @@ class ColorPieSquareComponent extends HTMLElement {
         this.render()
     }
 }
+class ColorPie {
+    constructor(x,y,r) {
+        this.x = x
+        this.y = y
+        this.r = r
+    }
+    draw(context,scale) {
+        context.fillStyle = '#E65100'
+        context.save()
+        context.translate(this.x,this.y)
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var i=0;i<Math.floor(360*this.scale);i+=10) {
+            const x = this.r*Math.cos(i*Math.PI/180),y = this.r*Math.sin(i*Math.PI/180)
+            context.lineTo(x,y)
+        }
+        context.fill()
+        context.restore()
+    }
+    handleTap(x,y) {
+        return x>=this.x-this.r && x<=this.x+this.r && y>=this.y-this.r && y<=this.y+this.r
+    }
+}
