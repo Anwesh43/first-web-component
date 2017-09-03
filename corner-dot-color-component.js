3861b242fa9e5050a78e5bdb50d773ff48f30d39
Anwesh43 <anweshthecool0@gmail.com>
2017-09-03 14:46:18 +0530

    adding CornerDotColor class

--- corner-dot-color-component.js
+++ corner-dot-color-component.js
@@ -17,3 +17,62 @@ class CornerDotColorComponent extends HTMLElement {
         this.render()
     }
 }
+//This the logic to create that four circles
+class CornerDotColor {
+    constructor() {
+        this.x = size/2
+        this.y = size/2
+        this.r = size/10
+    }
+    drawStrokeCircle(context) {
+        context.beginPath()
+        context.arc(0,0,this.r,0,2*Math.PI)
+        context.stroke()
+    }
+    drawFillCircle(context) {
+        context.save()
+        context.scale(0,0)
+        context.beginPath()
+        context.arc(0,0,this.r,0,2*Math.PI)
+        context.fill()
+        context.restore()
+    }
+    drawArc(context) {
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var deg=0;deg<=360;deg+=5) {
+            const x = this.r*Math.cos(deg*Math.PI/180),y = this.r*Math.sin(deg*Math.PI/180)
+            context.lineTo(x,y)
+        }
+        context.fill()
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        context.lineWidth = size/40
+        context.strokeStyle = 'blue'
+        this.drawStrokeCircle(context)
+        this.drawFillCircle(context)
+        for(var i=0;i<4;i++) {
+            context.save()
+            context.rotate(i*Math.PI/2)
+            context.save()
+            context.translate(-size/3,-size/3)
+            this.drawStrokeCircle(context,-size/3,-size/3)
+            this.drawArc(context)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+    update() {
+
+    }
+    stopUpdating() {
+
+    }
+    startUpdating() {
+
+    }
+}
+// This about the webcomponent
