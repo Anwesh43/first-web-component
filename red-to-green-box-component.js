fd5f86d85dcae2956e358bc5e5d5fa78a25c8a9f
Anwesh43 <anweshthecool0@gmail.com>
2017-09-03 06:37:27 +0530

    creating Animator

--- red-to-green-box-component.js
+++ red-to-green-box-component.js
@@ -109,3 +109,23 @@ class State {
         return dir == 0
     }
 }
+class Animator  {
+    constructor(component) {
+        this.animated = false
+        this.component = component
+    }
+    startAnimation() {
+        if(!this.animated) {
+            this.animated = true
+            this.component.startUpdating()
+            const interval = setInterval(()=>{
+                this.component.render()
+                this.component.update()
+                if(this.component.stopped()) {
+                    this.animated = false
+                    clearInterval(interval)
+                }
+            },50)
+        }
+    }
+}
