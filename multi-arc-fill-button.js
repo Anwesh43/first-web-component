8fb7877c27703b444411162c0029107a4c763969
Anwesh43 <anweshthecool0@gmail.com>
2017-09-10 16:56:59 +0530

    adding State

--- multi-arc-fill-button.js
+++ multi-arc-fill-button.js
@@ -59,3 +59,26 @@ class MultiArcFillButton {
         return false
     }
 }
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+    }
+    update() {
+        this.scale = this.dir * 0.1
+        if(this.scale > 1) {
+            this.dir = 0
+            this.scale = 1
+        }
+        if(this.scale < 0) {
+            this.scale = 0
+            this.dir = 0
+        }
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    startUpdating() {
+        this.dir = 1 - 2*this.scale
+    }
+}
