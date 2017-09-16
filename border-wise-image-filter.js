f3cc764c1d2a59b7fd67ec667dcc74b230617696
Anwesh43 <anweshthecool0@gmail.com>
2017-09-16 19:26:20 +0530

    adding BorderWiseImageFilterRect

--- border-wise-image-filter.js
+++ border-wise-image-filter.js
@@ -1,3 +1,5 @@
+const colors = ["#00BCD4","#009688","#f44336","#E65100"]
+const x_factors = [0,1,0,1],y_factors = [0,0,1,1]
 class BorderWiseImageFilterComponent extends HTMLElement{
     constructor() {
         super()
@@ -18,5 +20,43 @@ class BorderWiseImageFilterComponent extends HTMLElement{
         this.image.onload = () => {
             this.render()
         }
+    }
+}
+class BorderWiseImageFilterRect {
+    constructor(index) {
+        this.index = index
+    }
+    draw(context,w,h) {
+        const x_factor = x_factors[this.index],y_factor = y_factors[this.index]
+        context.save()
+        context.globalAlpha = 0.55
+        context.fillStyle = color
+        context.translate(x_factor*w,y_factor*h)
+        context.scale(x_factor,y_factor)
+        context.fillRect(-this.w*(x_factor),-this.h*(y_factor),this.w,this.h)
+        const offset = (factor,r)=> (1 - 2*factor)*r
+        const r = w/10,offset_x = offset(x_factor,r),offset_y = offset(y_factor,r)
+        context.save()
+        context.translate(offset_x,offset_y)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.rotate(i*Math.PI/2+Math.PI/4)
+            context.beginPath()
+            context.moveTo(0,-2*r/3)
+            context.lineTo(0,2*r/3)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+        context.restore()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+
     }
 }
