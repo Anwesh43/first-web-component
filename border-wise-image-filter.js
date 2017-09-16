87345a3e49d47ace808d452b0d59b943126f3211
Anwesh43 <anweshthecool0@gmail.com>
2017-09-16 20:25:09 +0530

    adding minor fixes to draw the button

--- border-wise-image-filter.js
+++ border-wise-image-filter.js
@@ -11,9 +11,12 @@ class BorderWiseImageFilterComponent extends HTMLElement{
         this.animator = new Animator(this)
     }
     render() {
+        const w = this.image.width,h = this.image.height
         const canvas = document.createElement('canvas')
-        canvas.width = this.image.width
-        canvas.height = this.image.height
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.drawImage(this.image,0,0,w,h)
         if(!this.filters) {
             this.filters = colors.map((color,index)=>new BorderWiseImageFilterRect(index))
         }
@@ -29,7 +32,7 @@ class BorderWiseImageFilterComponent extends HTMLElement{
         }
         this.img.onmousedown = (event) => {
             if(this.filters) {
-                for(let filter of filters) {
+                for(let filter of this.filters) {
                     if(filter.handleTap(event.offsetX,event.offsetY)) {
                         this.animator.startAnimation(filter)
                         break
@@ -46,22 +49,31 @@ class BorderWiseImageFilterRect {
     }
     draw(context,w,h) {
         const x_factor = x_factors[this.index],y_factor = y_factors[this.index]
+        const color = colors[this.index]
         context.save()
-        context.globalAlpha = 0.55
         context.fillStyle = color
         context.translate(x_factor*w,y_factor*h)
+        context.save()
         context.scale(this.state.scale,this.state.scale)
-        context.fillRect(-this.w*(x_factor),-this.h*(y_factor),this.w,this.h)
+        context.globalAlpha = 0.55
+        context.fillRect(-w*(x_factor),-h*(y_factor),w,h)
+        context.restore()
         const offset = (factor,r)=> (1 - 2*factor)*r
-        const r = w/10,offset_x = offset(x_factor,r),offset_y = offset(y_factor,r)
+        const r = w/20,offset_x = offset(x_factor,r),offset_y = offset(y_factor,r)
         context.save()
         context.translate(offset_x,offset_y)
+        context.fillStyle = '#212121'
+        context.beginPath()
+        context.arc(0,0,r,0,2*Math.PI)
+        context.fill()
+        context.strokeStyle = 'white'
+        context.lineWidth = r/6
         for(var i=0;i<2;i++) {
             context.save()
-            context.rotate(i*Math.PI/2+Math.PI/4)
+            context.rotate(i*Math.PI/2+Math.PI/4*this.state.scale)
             context.beginPath()
-            context.moveTo(0,-2*r/3)
-            context.lineTo(0,2*r/3)
+            context.moveTo(0,-r)
+            context.lineTo(0,r)
             context.stroke()
             context.restore()
         }
@@ -92,7 +104,7 @@ class State {
         this.dir = 0
     }
     update() {
-        this.scale += this.dir * 0.1
+        this.scale += (this.dir * 0.1)
         if(this.scale > 1) {
             this.scale = 1
             this.dir = 0
@@ -106,7 +118,7 @@ class State {
         return this.dir == 0
     }
     startUpdating() {
-        this.scale = 1 - 2*this.dir
+        this.dir = 1 - 2*this.scale
     }
 }
 class Animator {
@@ -123,6 +135,7 @@ class Animator {
                 filter.update()
                 if(filter.stopped()) {
                     this.animated = false
+                    clearInterval(interval)
                 }
             },50)
         }
