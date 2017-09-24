843e17e33671b7b07fd6a3fd66e7560754c57ecf
Anwesh43 <anweshthecool0@gmail.com>
2017-09-24 20:17:42 +0530

    adding state

--- arc-switch-bar-component.js
+++ arc-switch-bar-component.js
@@ -1,4 +1,5 @@
 const w = window.innerWidth,h = window.innerHeight
+const color = "#FFC107"
 class ArcSwitchBarComponent extends HTMLElement {
     constructor() {
         super()
@@ -15,5 +16,75 @@ class ArcSwitchBarComponent extends HTMLElement {
     }
     connectedCallback() {
         this.render()
+    }
+}
+class ArcSwitchBar {
+    constructor(i,w,h) {
+        this.index
+        this.x = w/10 + (this.w*9/10)*i
+        this.y = h*0.95
+        this.oy = this.y
+        this.cx = w/2
+        this.cy = h/2
+        this.r = Math.min(w,h)/20
+        this.cr = Math.min(w,h)/5
+        this.maxH = 0.9*h
+    }
+    draw(context) {
+        context.fillStyle = color
+        context.strokeStyle = color
+        context.lineWidth = this.r/6
+        this.y = this.oy - this.maxH
+        const deg = this.index*180
+        context.save()
+        context.translate(this.cx,this.cy)
+        context.beginPath()
+        for(var i=deg;i<deg+180;i+=5) {
+            const x = this.r*Math.cos(i*Math.PI/180),y = this.r*Math.sin(i*Math.PI/180)
+            if(i == deg) {
+                context.moveTo(x,y)
+            }
+            else {
+                context.lineTo(x,y)
+            }
+        }
+        context.fill()
+        context.restore()
+        context.beginPath()
+        context.arc(this.x,this.y,this.r,0,2*Math.PI)
+        context.fill()
+        context.beginPath()
+        context.moveTo(this.x,this.y)
+        context.lineTo(this.x,this.oy)
+        context.stroke()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+
+    }
+}
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+    }
+    update() {
+        this.scale += this.dir*0.1
+        if(this.scale > 1) {
+            this.dir = 0
+            this.scale = 1
+        }
+        if(this.scale < 0) {
+            this.dir = 0
+            this.scale = 0
+        }
+    }
+    startUpdating() {
+        this.dir = 1-2*this.scale
     }
 }
