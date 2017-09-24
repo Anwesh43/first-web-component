1667d0e9a0dd8b68de8a7e25c1e9e6d14d7b3111
Anwesh43 <anweshthecool0@gmail.com>
2017-09-24 19:28:50 +0530

    adding ArcSwitchBarComponent methods

--- arc-switch-bar-component.js
+++ arc-switch-bar-component.js
@@ -1,11 +1,19 @@
+const w = window.innerWidth,h = window.innerHeight
 class ArcSwitchBarComponent extends HTMLElement {
     constructor() {
         super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
     }
     render() {
-
+        const canvas = document.createElement('canvas')
+        canvas.width = Math.min(w,h)/2
+        canvas.height = Math.min(w,h)/2
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
     }
     connectedCallback() {
-
+        this.render()
     }
 }
