6d9380aa5a858c370e3139f08ebbeef40753bf6b
Anwesh43 <anweshthecool0@gmail.com>
2017-10-01 18:53:52 +0530

    adding VaryingRadiusCircleComponent

---
+++ varying-radius-circle-component.js
@@ -0,0 +1,19 @@
+const lw = canvas.width,lh = canvas.height, size = Math.min(lw,lh)/4
+class VaryingRadiusCircleComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
