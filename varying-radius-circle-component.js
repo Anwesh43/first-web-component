f879e0cf66aa535048ad9d8bf861d318eb369f70
Anwesh43 <anweshthecool0@gmail.com>
2017-10-01 19:07:42 +0530

    adding VaryingRadiusCircleState

--- varying-radius-circle-component.js
+++ varying-radius-circle-component.js
@@ -53,3 +53,26 @@ class VaryingRadiusCircle {
 
     }
 }
+class VaryingRadiusState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+    }
+    update() {
+        this.scale += this.dir * 0.1
+        if(this.scale > 1) {
+            this.scale = 1
+            this.dir = 0
+        }
+        if(this.scale < 0) {
+            this.scale = 0
+            this.dir = 0
+        }
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    startUpdating() {
+        this.dir = 1-2*this.scale
+    }
+}
