73b8f1a808c9870f7f8f86173a42d8419da99555
Anwesh43 <anweshthecool0@gmail.com>
2017-10-07 13:17:03 +0530

    Adding DirectionButtonScaler

--- FourDirectionButtonScalerComponent.js
+++ FourDirectionButtonScalerComponent.js
@@ -16,3 +16,38 @@ class FourDirectionButtonScalerComponent extends HTMLElement{
         this.img.src = canvas.toDataURL()
     }
 }
+class DirectionButtonScaler {
+    constructor(i) {
+        this.x = w/2
+        this.y = h/2
+        this.r = size/15
+        this.maxH = size/3
+        this.x += (this.maxH+this.r)*(Math.cos(i*Math.PI/2))
+        this.y += (this.maxH+this.r)*(Math.sin(i*Math.PI/2))
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        context.beginPath()
+        context.arc(0,0,this.r,0,2*Math.PI)
+        context.stroke()
+        this.drawArc(context)
+        context.fillRect(-this.r,this.r,2*this.r,this.maxH)
+        context.restore()
+    }
+    drawArc(context) {
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var i=0;i<360;i+=5) {
+            const x = this.r*Math.cos(i*Math.PI/180),y = this.r*Math.sin(i*Math.PI/180)
+            context.lineTo(x,y)
+        }
+        context.fill()
+    }
+    update() {
+
+    }
+    stopped() {
+        return false
+    }
+}
