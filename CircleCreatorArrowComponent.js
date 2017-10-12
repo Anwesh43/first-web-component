ec334a5751bf6625abb0786eff461e04a769b415
Anwesh43 <anweshthecool0@gmail.com>
2017-10-12 20:59:30 +0530

    adding CircleCreatorArrow

--- CircleCreatorArrowComponent.js
+++ CircleCreatorArrowComponent.js
@@ -17,3 +17,48 @@ class CircleCreatorArrowComponent extends HTMLElement {
         this.render()
     }
 }
+class CircleCreatorArrow {
+    constructor() {
+
+    }
+    draw(context) {
+        context.save()
+        context.translate(size/2,size/2)
+        this.drawTriangle(context)
+        context.beginPath()
+        for(var i=0;i<360;i+=20) {
+            this.drawPointInCircle(context,i)
+        }
+        context.stroke()
+        context.restore()
+    }
+    drawPointInCircle(context,deg) {
+        const x = (size/3)*Math.cos(deg*Math.PI/180),y = (size/3)*Math.sin(deg*Math.PI/180)
+        if(deg == 0) {
+            context.moveTo(x,y)
+        }
+        else {
+            context.lineTo(x,y)
+        }
+    }
+    drawTriangle(context) {
+        context.save()
+        context.translate(0,-size/3)
+        context.scale(1,1)
+        context.beginPath()
+        context.moveTo(-size/20,-size/20)
+        context.lineTo(-size/20,size/20)
+        context.lineTo(size/20,0)
+        context.fill()
+        context.restore()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+        return false
+    }
+}
