1318a8cbca0716d4a9738a95f00f75f201a3b84b
Anwesh43 <anweshthecool0@gmail.com>
2017-10-13 18:45:22 +0530

    adding CornerImageFilterScaler

--- CornerImageFilterScalerComponent.js
+++ CornerImageFilterScalerComponent.js
@@ -22,3 +22,28 @@ class CornerImageFilterScalerComponent extends HTMLElement {
         }
     }
 }
+class CornerImageFilterScaler {
+    constructor(i) {
+        this.i = i
+        this.x = (size/3)*Math.cos(i*Math.PI/2)
+        this.y = (size/3)*Math.sin(i*Math.PI/2)
+    }
+    draw(context,image) {
+        context.save()
+        context.translate(this.x,this.y)
+        context.beginPath()
+        context.restore()
+    }
+    drawCircle(context) {
+        const r = size/15
+        context.moveTo(0,0)
+        for(var i=0;i<=360;i+=10) {
+            const x = r*Math.cos(i*Math.PI/180),y = r*Math.sin(i*Math.PI/180)
+            context.lineTo(x,y)
+        }
+        context.fill()
+    }
+    clipImage(context,image) {
+
+    }
+}
