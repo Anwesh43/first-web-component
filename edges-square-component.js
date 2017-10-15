3ece75f732ca563558c32894c4fa6d61bb1535b5
Anwesh43 <anweshthecool0@gmail.com>
2017-10-15 21:21:05 +0530

    adding EdgesSquareComponent

---
+++ edges-square-component.js
@@ -0,0 +1,19 @@
+const w = window.innerWidth,h = window.innerHeight, size = Math.min(w,h)/2
+class EdgesSquareComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
