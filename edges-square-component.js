d016f68cb28ca1b110036612003c609ce2ee7105
Anwesh43 <anweshthecool0@gmail.com>
2017-10-15 21:57:38 +0530

    adding EdgeSquare class

--- edges-square-component.js
+++ edges-square-component.js
@@ -17,3 +17,41 @@ class EdgesSquareComponent extends HTMLElement {
         this.render()
     }
 }
+class EdgeSquare {
+    constructor(i) {
+        this.i = i
+        this.x = size/2 + (-size/3+(size/3)*(i%2))
+        this.y = size/2 + (-size/3+(size/3)*Math.floor(i/2))
+        if(i < 3) {
+            this.next = new EdgeSquare(i+1)
+        }
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        context.strokeRect(-size/10,-size/10,size/5,size/5)
+        context.save()
+        context.scale(1,1)
+        context.fillRect(-size/10,-size/10,size/5,size/5)
+        context.restore()
+        context.restore()
+        context.beginPath()
+        context.moveTo(this.x,this.y)
+        if(this.next) {
+            context.lineTo(this.next.x,this.next.y)
+        }
+        else {
+            context.lineTo(this.x+2*size/3,this.y)
+        }
+        context.stroke()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+        
+    }
+}
