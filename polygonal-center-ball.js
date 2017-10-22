99b9c65f3ffa6dcdd78848f4158ea3c7ec368efa
Anwesh43 <anweshthecool0@gmail.com>
2017-10-22 21:49:27 +0530

    adding PolygonalCenterBall

--- polygonal-center-ball.js
+++ polygonal-center-ball.js
@@ -1,4 +1,11 @@
 const w = window.innerWidth,h = window.innerHeight,size = Math.min(w,h)*0.4
+const attachCustomFunctionality = (context) {
+    context.fillCircle = function(x,y,r) {
+        context.beginPath()
+        context.arc(x,y,r,0,2*Math.PI)
+        context.fill()
+    }
+}
 class PolygonalCenterBallComponent extends HTMLElement {
     constructor() {
         super()
@@ -12,9 +19,24 @@ class PolygonalCenterBallComponent extends HTMLElement {
         canvas.width = size
         canvas.height = size
         const context = canvas.getContext('2d')
+        attachCustomFunctionality(context)
         this.img.src = canvas.toDataURL()
     }
     connectedCallback() {
         this.render()
+    }
+}
+class PolygonalCenterBall {
+    draw(context,n) {
+        context.save()
+        context.translate(size/2,size/2)
+
+        context.restore()
+    }
+    drawBalls(context,n) {
+        for(var i=0;i<n;i++) {
+            const x = (size/3)*Math.cos(i*(2*Math.PI/n)),y = (size/3)*Math.sin(i*(2*Math.PI/n))
+            context.fillCircle(x,y,size/20)
+        }
     }
 }
