4b112e61176eb672110941843acbe9a93f0cff7d
Anwesh43 <anweshthecool0@gmail.com>
2017-10-24 22:37:12 +0530

    adding ColorFilterState

--- four-color-filter.js
+++ four-color-filter.js
@@ -64,3 +64,26 @@ class ColorFilter {
 
     }
 }
+class ColorFilterState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+    }
+    update() {
+        this.scale += this.dir*0.1
+        if(this.scale > 1) {
+            this.scale = 1
+            this.dir = 0
+        }
+        if(this.scale < 0) {
+            this.scale = 0
+            this.dir = 0
+        }
+    }
+    startUpdating() {
+        this.dir = 1-2*this.scale
+    }
+    stopped() {
+        return this.dir == 0
+    }
+}
