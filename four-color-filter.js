d7d280f3f54c25591dc41bf263d4ff45f2bad889
Anwesh43 <anweshthecool0@gmail.com>
2017-10-24 19:25:57 +0530

    adding ColorFilter

--- four-color-filter.js
+++ four-color-filter.js
@@ -1,5 +1,21 @@
 const w = window.innerWidth,h = window.innerHeight,size = Math.min(w,h)/2
 const colors = ["#00838F","#E65100","#6A1B9A","#e53935"]
+const attachFunctionalityToContext = (context) => {
+    context.fillCircleOnScale = function(x,y,r,scale) {
+        context.save()
+        context.save(x,y)
+        context.beginPath()
+        context.arc(0,0,r,0,2*Math.PI)
+        context.stroke()
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var i=0;i<=360*scale;i+=5) {
+            context.lineTo(r*Math.cos(i*Math.PI/180),r*Math.sin(i*Math.PI/180))
+        }
+        context.fill()
+        context.restore()
+    }
+}
 class FourColorFilterComponent extends HTMLElement {
     constructor() {
         super()
@@ -13,6 +29,7 @@ class FourColorFilterComponent extends HTMLElement {
         canvas.width = size
         canvas.height = size
         const context = canvas.getContext('2d')
+        attachFunctionalityToContext(context)
         this.img.src = canvas.toDataURL()
     }
     connectedCallback() {
@@ -21,5 +38,29 @@ class FourColorFilterComponent extends HTMLElement {
         this.image.onload = () => {
             this.render()
         }
+    }
+}
+class ColorFilter {
+    constructor(i) {
+        this.i = i
+        this.color = colors[i]
+        this.x = (size/3)*Math.cos(i*Math.PI/2+Math.PI/4)
+        this.y = (size/3)*Math.sin(i*Math.PI/2+Math.PI/4)
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        context.fillCircleOnScale(0,0,size/12,1)
+        context.fillRect(-this.x,-this.y,size/3,size/3)
+        context.restore()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+
     }
 }
