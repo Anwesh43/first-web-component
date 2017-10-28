ce5f21f557f6b9c16122399cf33b9be0503afe78
Anwesh43 <anweshthecool0@gmail.com>
2017-10-28 13:38:07 +0530

    adding InMoveRectComponent

--- move_in_rect_component.js
+++ move_in_rect_component.js
@@ -57,3 +57,24 @@ class InRectMoverNode {
 
     }
 }
+class InRectMoverNodeState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update() {
+        this.scale += 0.1*dir
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = (this.prevScale+1)%2
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    startUpdating() {
+        this.dir = 1-2*this.scale
+    }
+}
