b64a056793d808a2587243446401c6710100b7b1
Anwesh43 <anweshthecool0@gmail.com>
2017-10-29 18:07:30 +0530

    adding DividingCircle

--- dividing-circle-component.js
+++ dividing-circle-component.js
@@ -1,4 +1,9 @@
-var w = window.innerWidth,h = window.innerHeight
+var w = window.innerWidth,h = window.innerHeight,size = Math.min(w,h)/2
+const drawCircle = (context,x,y,r) => {
+    context.beginPath()
+    context.arc(x,y,r,0,2*Math.PI)
+    context.fill()
+}
 class DividingCircleComponent extends HTMLElement {
     constructor() {
         super()
@@ -11,9 +16,50 @@ class DividingCircleComponent extends HTMLElement {
     }
     render() {
         const canvas = document.createElement('canvas')
-        canvas.width = w
-        canvas.height = h
+        canvas.width = size
+        canvas.height = size
         const context = canvas.getContext('2d')
         this.img.src = canvas.toDataURL()
+    }
+}
+class DividingCircle {
+    constructor() {
+
+    }
+    drawLine(context) {
+        context.beginPath()
+        context.moveTo(0,0)
+        context.lineTo(size/6,0)
+        context.stroke()
+    }
+    draw(context) {
+        context.save()
+        context.translate(size/2,size/2)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.scale(1-2*i,1)
+            this.drawLine(context)
+            context.save()
+            context.translate(size/6,0)
+            for(var j=0;j<2;j++) {
+                context.save()
+                context.rotate(Math.PI/4*(1-2*j))
+                this.drawLine(context)
+                drawCircle(size/6,0,size/20)
+                context.restore()
+            }
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+    update() {
+
+    }
+    startUpdating() {
+
+    }
+    stopped() {
+
     }
 }
