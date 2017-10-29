8b84bfc0e1aef812fad3358f60fa756a3d39e7d9
Anwesh43 <anweshthecool0@gmail.com>
2017-10-29 17:07:54 +0530

    adding DividingCircleComponent

---
+++ dividing-circle-component.js
@@ -0,0 +1,19 @@
+var w = window.innerWidth,h = window.innerHeight
+class DividingCircleComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    connectedCallback() {
+        this.render()
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+}
