25ad14741af377b0bd9f35c139594eda7f5ae37b
Anwesh43 <anweshthecool0@gmail.com>
2017-11-17 18:45:27 +0530

    adding ArcReactorComponent

---
+++ arc-reactor-component.js
@@ -0,0 +1,18 @@
+const w = window.innerWidth,h = window.innerHeight
+class ArcReactorComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0,0,w,h)
+        this.img.src = canvas.toDataURL()
+    }
+}
