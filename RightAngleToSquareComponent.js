d1c28e59d3f77fa30da1260eaac0520d00e87bb9
Anwesh43 <anweshthecool0@gmail.com>
2017-11-21 16:21:38 +0530

    adding RightAngleToSquare

--- RightAngleToSquareComponent.js
+++ RightAngleToSquareComponent.js
@@ -17,3 +17,37 @@ class RightAngleToSquareComponent extends HTMLElement {
         this.render()
     }
 }
+class RightAngleToSquare {
+    constructor(size) {
+        this.size = size
+        this.deg = 0
+        this.scale = 0
+        this.updateFns = [(scale)=>{
+            this.scale = scale
+        },(scale)=>{
+            this.deg = Math.PI*scale
+        }]
+    }
+    draw(context) {
+        context.save()
+        context.translate(w/2,h/2)
+        context.lineWidth = Math.min(w,h)/40
+        context.lineCap = "round"
+        for(var i=0;i<2;i++) {
+            canvas.save()
+            canvas.rotate(i*this.deg)
+            for(var j=0;j<2;j++) {
+                context.save()
+                context.translate(size/2,size/2)
+                context.rotate(-j*Math.PI/2)
+                context.beginPath()
+                context.moveTo(0,0)
+                context.lineTo(0,-size*this.scale)
+                context.stroke()
+                context.restore()
+            }
+            canvas.restore()
+        }
+        context.restore()
+    }
+}
