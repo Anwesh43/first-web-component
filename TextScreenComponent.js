2d93539b091c93cece6bc559e0216a4710e7763b
Anwesh43 <anweshthecool0@gmail.com>
2017-11-24 11:35:38 +0530

    adding TextScreenContainer

---
+++ TextScreenComponent.js
@@ -0,0 +1,19 @@
+const w = window.innerWidth,h = window.innerHeight
+class TextScreenComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('canvas')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
