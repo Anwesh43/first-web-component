e9503e8c02568607192b755c9aa1ca0f77a6f643
Anwesh43 <anweshthecool0@gmail.com>
2017-11-25 13:30:09 +0530

    adding RoundBarButtonListComponent

---
+++ RoundBarButtonListComponent.js
@@ -0,0 +1,20 @@
+const w = canvas.width,h = canvas.height
+class RoundBarButtonListComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0,0,w,h)
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
