337059294a833f6c1ed68c37a2db9ce0dd75f5f4
Anwesh43 <anweshthecool0@gmail.com>
2017-12-09 21:29:29 +0530

    adding LineStepContainerState

--- LineStepComponent.js
+++ LineStepComponent.js
@@ -55,3 +55,23 @@ class LineStepContainer{
 
     }
 }
+class LineStepContainerState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        this.dir = 1-2*this.scale
+        startcb()
+    }
+}
