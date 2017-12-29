060b034d5f9571e4699e5083571dee16d3ff47b5
Anwesh43 <anweshthecool0@gmail.com>
2017-12-29 16:03:21 +0530

    adding ScreenFillerComponent

---
+++ ScreenFillerComponent.js
@@ -0,0 +1,12 @@
+class ScreenFillerComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+    }
+    render() {
+
+    }
+    connectedCallback() {
+        
+    }
+}
