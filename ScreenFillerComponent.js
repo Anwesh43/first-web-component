1f6bca23171db9ef129a9394238c46cd4ff717a3
Anwesh43 <anweshthecool0@gmail.com>
2017-12-29 16:51:42 +0530

    adding Circle

--- ScreenFillerComponent.js
+++ ScreenFillerComponent.js
@@ -1,4 +1,10 @@
 const w = window.innerWidth,h = window.innerHeight
+const createCircularDiv = (div) => {
+    div.style.position = 'absolute'
+    div.style.left = w/2-w/15
+    div.style.top = 4*h/5
+    div.style.borderRadius = '50%'
+}
 class ScreenFillerComponent extends HTMLElement {
     constructor() {
         super()
@@ -27,5 +33,28 @@ class ScreenFiller {
     }
     update(scale) {
         this.div.style.width = w*scale
+    }
+}
+class Circle {
+    constructor(shadow) {
+        this.createDom(shadow)
+    }
+    createDom(shadow) {
+        this.div = document.createElement('div')
+        this.backDiv = document.createElement('div')
+        createCircularDiv(this.div)
+        createCircularDiv(this.backDiv)
+        this.div.style.background = 'yellow'
+        this.backDiv.style.border = '5px solid yellow'
+        this.backDiv.style.width = 2*w/15
+        this.backDiv.style.height = 2*w/15
+        this.div.style.height = 0
+        this.div.style.width = 0
+        shadow.appendChild(this.div)
+        shadow.appendChild(this.backDiv)
+    }
+    update(scale) {
+        this.div.style.width = (2*w/15)*scale
+        this.div.style.height = (2*w/15)*scale
     }
 }
