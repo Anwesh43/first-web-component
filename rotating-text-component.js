498c3e093d740dabcbe7630577bf94fe951f81c7
Anwesh43 <anweshthecool0@gmail.com>
2017-12-29 14:36:51 +0530

    adding RotatingTextComponent

---
+++ rotating-text-component.js
@@ -0,0 +1,18 @@
+const w = window.innerWidth,h = window.innerHeight, size = Math.min(w,h)/3
+class RotatingTextComponent extends HTMLElement {
+    constructor(){
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
