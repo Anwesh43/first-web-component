e2e71543b2b7446fcf258791842e789fab896191
Anwesh43 <anweshthecool0@gmail.com>
2017-12-29 16:17:05 +0530

    adding ScreenFiller

--- ScreenFillerComponent.js
+++ ScreenFillerComponent.js
@@ -1,3 +1,4 @@
+const w = window.innerWidth,h = window.innerHeight
 class ScreenFillerComponent extends HTMLElement {
     constructor() {
         super()
@@ -8,5 +9,23 @@ class ScreenFillerComponent extends HTMLElement {
     }
     connectedCallback() {
         this.render()
+    }
+}
+class ScreenFiller {
+    constructor(shadow) {
+        this.createDom(shadow)
+    }
+    createDom(shadow) {
+        this.div = document.createElement('div')
+        this.div.style.width = 0
+        this.div.style.position = 'absolute'
+        this.div.style.top = 0
+        this.div.style.left = 0
+        this.div.style.height = 3*h/5
+        this.div.style.background = 'yellow'
+        shadow.appendChild(this.div)
+    }
+    update(scale) {
+        this.div.style.width = w*scale
     }
 }
