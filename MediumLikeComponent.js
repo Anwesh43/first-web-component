4928d008ddff55a862cc998bbafea23af58d2b75
Anwesh43 <anweshthecool0@gmail.com>
2017-12-31 16:57:40 +0530

    adding MediumLikeComponent

---
+++ MediumLikeComponent.js
@@ -0,0 +1,20 @@
+const w = window.innerWidth, h = window.innerHeight,size = Math.min(w,h)/3
+class MediumLikeComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
