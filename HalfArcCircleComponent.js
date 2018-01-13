84a79ac95627cb2464ac30fa4fb95cb01cffe205
Anwesh43 <anweshthecool0@gmail.com>
2018-01-13 10:49:39 +0530

    adding HalfArcCircleComponent

--- HalfArcCircleComponent.js
+++ HalfArcCircleComponent.js
@@ -17,3 +17,40 @@ class HalfArcCircleComponent extends HTMLElement {
         this.render()
     }
 }
+class HalfArcCircle {
+    constructor(x,y,r) {
+        this.x = x
+        this.y = y
+        this.r = r
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.scale(i*2-1,1)
+            this.drawArc(context)
+            context.restore()
+        }
+        context.restore()
+    }
+    drawArc(context) {
+        context.beginPath()
+        for(var i=0;i<=180;i++) {
+            const x = this.r*Math.cos(i*Math.PI/180), y = this.r*Math.sin(i*Math.PI/180)
+            if(i == 0) {
+                context.moveTo(x,y)
+            }
+            else {
+                context.lineTo(x,y)
+            }
+        }
+        context.stroke()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(startcb) {
+
+    }
+}
