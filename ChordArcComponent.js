73b42aa6ab027795610393207fd4987f6a97364a
Anwesh43 <anweshthecool0@gmail.com>
2018-01-20 23:11:53 +0530

    adding ChordArc class

--- ChordArcComponent.js
+++ ChordArcComponent.js
@@ -1,4 +1,4 @@
-const w = window.innerWidth, h = window.innerHeight
+const w = window.innerWidth, h = window.innerHeight,size = Math.min(w,h)/3
 class ChordArcComponent extends HTMLElement {
     constructor() {
         super()
@@ -10,10 +10,38 @@ class ChordArcComponent extends HTMLElement {
         const canvas = document.createElement('canvas')
         const context = canvas.getContext('2d')
         context.fillStyle = '#212121'
-        context.fillRect(0,0,w,h)
+        context.fillRect(0,0,size,size)
         this.img.src = canvas.toDataURL()
     }
     connectedCallback() {
         this.render()
     }
+}
+class ChordArc {
+    constructor(i) {
+        this.i = i
+    }
+    draw(context) {
+        context.save()
+        context.translate(size/2,size/2)
+        context.beginPath()
+        for(var i = -45;i<=45;i++) {
+            const x = (size/3)*Math.cos(i*Math.PI/180),y = (size/3)*Math.sin(i*Math.PI/180)
+            if(i == -45) {
+                context.moveTo(x,y)
+            }
+            else {
+                context.lineTo(x,y)
+            }
+        }
+        context.fill()
+        context.restore()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(startcb) {
+
+    }
+
 }
