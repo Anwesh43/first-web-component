f9f15903ebac100bf703bcd49aac04df24a47cf8
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 00:27:28 +0530

    adding fixes in ChordArcComponent

--- ChordArcComponent.js
+++ ChordArcComponent.js
@@ -1,4 +1,4 @@
-const w = window.innerWidth, h = window.innerHeight,size = Math.min(w,h)/3
+const w = window.innerWidth, h = window.innerHeight,size = Math.min(w,h)/2
 class ChordArcComponent extends HTMLElement {
     constructor() {
         super()
@@ -10,9 +10,12 @@ class ChordArcComponent extends HTMLElement {
     }
     render() {
         const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
         const context = canvas.getContext('2d')
         context.fillStyle = '#212121'
         context.fillRect(0,0,size,size)
+        context.fillStyle = 'yellow'
         this.container.draw(context)
         this.img.src = canvas.toDataURL()
     }
@@ -38,7 +41,7 @@ class ChordArc {
     draw(context) {
         context.save()
         context.translate(size/2,size/2)
-        context.rotate(i*Math.PI/2+(Math.PI/2)*this.state.scale)
+        context.rotate(this.i*Math.PI/2+(Math.PI/2)*this.state.scale)
         context.beginPath()
         for(var i = -45;i<=45;i++) {
             const x = (size/3)*Math.cos(i*Math.PI/180),y = (size/3)*Math.sin(i*Math.PI/180)
@@ -66,7 +69,8 @@ class ChordArcState {
         this.dir = 0
     }
     update(stopcb) {
-        this.scale += 0.1
+        this.scale += 0.1*this.dir
+        console.log(this.scale)
         if(Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
@@ -90,35 +94,34 @@ class ChordArcContainer {
         }
     }
     draw(context) {
-        for(var i=0;i<4;i++) {
-            this.chordArcs.forEach((chordArc) => {
-                chordArc.draw(context)
-            })
+        for(var i=0;i<this.state.j+1;i++) {
+            this.chordArcs[i].draw(context)
         }
     }
     update(stopcb) {
-        if(this.j <= 4 || this.j >= 1) {
-            this.chordArc[this.j - 1].update(()=>{
+        if(this.state.j < 4 || this.state.j >= 0) {
+            this.chordArcs[this.state.j].update(()=>{
                 this.state.incrementJ()
                 stopcb()
             })
         }
     }
     startUpdating(startcb) {
-      if(this.j <= 4 || this.j >= 1) {
-          this.chordArc[this.j - 1].startUpdating(startcb)
+      if(this.state.j < 4 || this.state.j >= 0) {
+          this.chordArcs[this.state.j].startUpdating(startcb)
       }
     }
 }
 class ChordArcContainerState {
     constructor() {
         this.j = 0
-        this.dir = 0
+        this.dir = 1
     }
     incrementJ() {
         this.j+=this.dir
-        if(this.j == 4 || this.j == 0) {
-            this.dir = -1
+        if(this.j == 4 || this.j == -1) {
+            this.dir *= -1
+            this.j+=this.dir
         }
     }
 }
@@ -131,7 +134,7 @@ class Animator {
             this.animated = true
             this.interval = setInterval(()=>{
                 updatecb()
-            }),50
+            },50)
         }
     }
     stop() {
