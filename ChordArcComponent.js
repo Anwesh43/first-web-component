222401a9732f73129206779f9ff06fe348add5bc
Anwesh43 <anweshthecool0@gmail.com>
2018-01-20 20:00:07 +0530

    adding ChordArcComponent

---
+++ ChordArcComponent.js
@@ -0,0 +1,19 @@
+const w = window.innerWidth, h = window.innerHeight
+class ChordArcComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0,0,w,h)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
