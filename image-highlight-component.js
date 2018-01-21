af229145c0dc23790ef82426eed902497331f1b4
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 16:53:07 +0530

    adding ImageHighlightState

--- image-highlight-component.js
+++ image-highlight-component.js
@@ -45,3 +45,24 @@ class ImageHighlight {
 
     }
 }
+class ImageHighlightState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.scale
+        }
+    }
+}
