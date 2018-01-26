92888876301921da4f06f9e33068b25898a7480c
Anwesh43 <anweshthecool0@gmail.com>
2018-01-26 12:52:37 +0530

    adding AlternateBar

--- AlternateBarComponent.js
+++ AlternateBarComponent.js
@@ -1,4 +1,5 @@
 const w = window.innerWidth
+const alternatingBarColors = ['#673AB7','#FF5722']
 class AlternateBarComponent extends HTMLElement {
     constructor() {
         super()
@@ -17,5 +18,17 @@ class AlternateBarComponent extends HTMLElement {
         context.fillStyle = '#212121'
         context.fillRect(0,0,w,w)
         this.img.src = canvas.toDataURL()
+    }
+}
+class AlternateBar {
+    constructor(i) {
+        this.i = i
+    }
+    draw(context,x_size,y_size,scale) {
+        const x = x_size * this.i
+        const y = y_size*(1-scale)*(i%2)
+        const h = y_size*scale
+        context.fillStyle = alternatingBarColors[i%2]
+        context.fillRect(x,y,x_size,h)
     }
 }
