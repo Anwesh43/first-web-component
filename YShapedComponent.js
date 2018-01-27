ba1756c8a3da39a06446116cbc5a2681c62c070f
Anwesh43 <anweshthecool0@gmail.com>
2018-01-27 16:16:14 +0530

    adding YShapedAnimator

--- YShapedComponent.js
+++ YShapedComponent.js
@@ -72,3 +72,22 @@ class YShapedState {
         }
     }
 }
+class YShapedAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
