63be2cd9b4bfa6686e5237abd10bcb1600b5eb8a
Anwesh43 <anweshthecool0@gmail.com>
2018-01-27 16:07:00 +0530

    adding YShaped

--- YShapedComponent.js
+++ YShapedComponent.js
@@ -19,3 +19,34 @@ class YShapedComponent extends HTMLElement {
         this.render()
     }
 }
+class YShaped {
+    constructor() {
+
+    }
+    draw(context) {
+        context.strokeStyle = '#FDD835'
+        context.save()
+        context.translate(size/2,size/2)
+        context.beginPath()
+        context.moveTo(0,0)
+        context.lineTo(0,2*size/5)
+        context.stroke()
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.translate(0,0)
+            context.rotate(2*Math.PI/3*(i%2-1))
+            context.beginPath()
+            context.moveTo(0,0)
+            context.lineTo(0,size/4)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(startcb) {
+
+    }
+}
