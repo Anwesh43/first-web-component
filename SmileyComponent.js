c80543af7ce30309b4da766c86a4093312678a70
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 14:15:29 +0530

    adding State class

--- SmileyComponent.js
+++ SmileyComponent.js
@@ -17,3 +17,25 @@ class SmileyComponent extends HTMLElement {
         this.render()
     }
 }
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.prevScale = this.scale
+            this.dir = 0
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.scale
+            startcb()
+        }
+    }
+}
