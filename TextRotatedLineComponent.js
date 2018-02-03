7e5467c53d6255ef0b7b9dbc9f3f868bcf0de520
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 12:06:10 +0530

    adding Animator class

--- TextRotatedLineComponent.js
+++ TextRotatedLineComponent.js
@@ -84,3 +84,22 @@ class TextRotatedLine {
         this.state.startUpdating(startcb)
     }
 }
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            })
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
