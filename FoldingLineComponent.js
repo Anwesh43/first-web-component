fdc4712009408464a832e06de46daa1c6800a41f
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 12:07:10 +0530

    adding Animator

--- FoldingLineComponent.js
+++ FoldingLineComponent.js
@@ -39,3 +39,22 @@ class State {
         }
     }
 }
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
