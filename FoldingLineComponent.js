8a7d8523ac78bca5f30800bafa6fcc6ea802e386
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 11:59:19 +0530

    adding State class

--- FoldingLineComponent.js
+++ FoldingLineComponent.js
@@ -17,3 +17,25 @@ class FoldingLineComponent extends HTMLElement {
         this.img.src = canvas.toDataURL()
     }
 }
+class State {
+    constructor() {
+        this.dir = 0
+        this.scale = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.prevScale = this.scale + this.dir
+            this.dir = 0
+            this.scale = this.prevScale
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.scale
+            startcb()
+        }
+    }
+}
