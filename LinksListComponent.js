47548c673ee7cabc1c4452043afe5e97e6423221
Anwesh43 <anweshthecool0@gmail.com>
2018-02-10 21:37:27 +0530

    adding Animator class

--- LinksListComponent.js
+++ LinksListComponent.js
@@ -62,3 +62,22 @@ class LinkState {
         }
     }
 }
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
