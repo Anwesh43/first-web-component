5d78a90b803d7e64175489f3b5a98065f25f8b46
Anwesh43 <anweshthecool0@gmail.com>
2018-02-11 18:42:13 +0530

    adding DoubleBarAnimator class

--- DoubleBarComponent.js
+++ DoubleBarComponent.js
@@ -62,3 +62,22 @@ class DoubleBarState {
         }
     }
 }
+class DoubleBarAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
