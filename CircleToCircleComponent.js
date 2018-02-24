ec4edadd65242d0bbae69d405973f59b940cab32
Anwesh43 <anweshthecool0@gmail.com>
2018-02-24 12:38:24 +0530

    adding State class

--- CircleToCircleComponent.js
+++ CircleToCircleComponent.js
@@ -19,3 +19,32 @@ class CircleToCircleComponent extends HTMLElement {
         this.render()
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+        this.jDir = 1
+    }
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if(Math.abs(this.prevScale - this.scales[this.j]) > 1) {
+            this.scales[this.j] = 0
+            this.j += this.jDir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.jDir *= -1
+                this.dir = 0
+                this.j += this.jDir
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
