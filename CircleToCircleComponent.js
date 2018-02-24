860c1e9b5b915fbf3aaf23e429745032330ac981
Anwesh43 <anweshthecool0@gmail.com>
2018-02-24 12:29:35 +0530

    adding CircleToCircleComponent

---
+++ CircleToCircleComponent.js
@@ -0,0 +1,21 @@
+const w = window.innerWidth, h = window.innerHeight, size = Math.min(w, h) / 2
+class CircleToCircleComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode : 'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, w, h)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
