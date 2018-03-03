a669529270065a212bd565609b4430a0d45db630
Anwesh43 <anweshthecool0@gmail.com>
2018-03-03 15:35:55 +0530

    adding State class

--- CircleExpandComponent.js
+++ CircleExpandComponent.js
@@ -19,3 +19,34 @@ class CircleExpandComponent extends HTMLElement {
         this.render()
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0, 0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scales += this.dir * 0.1
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.dir *= -1
+                this.j += this.dir
+                this.prevScale = this.scales[this.j]
+                if(this.dir == 1) {
+                    this.j = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+
+}
