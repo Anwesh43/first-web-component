c0848f880a3b74e38df1291b20c5845be2ac0c16
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 12:20:36 +0530

    adding State class

--- TextUnderOverLineComponent.js
+++ TextUnderOverLineComponent.js
@@ -18,3 +18,30 @@ class TextUnderOverLineComponent extends HTMLElement {
         this.img.src = canvas.toDataURL()
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+    }
+    startUpdatig(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1){
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.lenght || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+}
