ad5ba9f516e96ba6a081f836757023b3b9c23de0
Anwesh43 <anweshthecool0@gmail.com>
2018-03-21 22:05:47 +0530

    adding State class

--- TicTacLineComponent.js
+++ TicTacLineComponent.js
@@ -19,3 +19,25 @@ class TicTacLineComponent extends HTMLElement {
         this.render()
     }
 }
+class State {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
