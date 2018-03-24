16bf1ac89755026e8f1aabbd626de0e7b68372ea
Anwesh43 <anweshthecool0@gmail.com>
2018-03-24 14:10:37 +0530

    adding CircleBallRotComponent

---
+++ CircleBallRotComponent.js
@@ -0,0 +1,22 @@
+const w = window.innerWidth, h = window.innerHeight, size = Math.min(w,h)/15
+class CircleBallRotComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        shadow.appendChild(this.img)
+    }
+    connectedCallback() {
+        this.render()
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, w, h)
+        this.img.src = canvas.toDataURL()
+
+    }
+}
