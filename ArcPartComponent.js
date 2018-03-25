bc39a7f9f6bc6d258ed67d232705abd989ec3fa0
Anwesh43 <anweshthecool0@gmail.com>
2018-03-25 19:32:14 +0530

    adding State class

--- ArcPartComponent.js
+++ ArcPartComponent.js
@@ -19,3 +19,23 @@ class ArcPartComponent extends HTMLElement {
         render()
     }
 }
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(startcb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+    startUpdating() {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+        }
+    }
+}
