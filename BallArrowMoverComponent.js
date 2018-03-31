08bfa5f97b0fbce223b91904859ff5efe43047fe
Anwesh43 <anweshthecool0@gmail.com>
2018-03-31 17:17:43 +0530

    adding BallArrowMoverComponent

---
+++ BallArrowMoverComponent.js
@@ -0,0 +1,21 @@
+const w = window.innerWidth, h = window.innerHeight
+class BallArrowMoverComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadpw({mode : 'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, w, h)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
