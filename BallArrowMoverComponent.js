74a09fcff7b81c1084bfce9b624420bae9b4531e
Anwesh43 <anweshthecool0@gmail.com>
2018-03-31 17:29:28 +0530

    adding BAMState class

--- BallArrowMoverComponent.js
+++ BallArrowMoverComponent.js
@@ -19,4 +19,26 @@ class BallArrowMoverComponent extends HTMLElement {
         this.render()
     }
 }
+class BAMState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
 customElements.define('ball-arrow-mover', BallArrowMoverComponent)
