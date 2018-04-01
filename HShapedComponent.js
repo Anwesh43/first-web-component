435ce1d1d46f15d5613a23ecd2978a3a5dec7c5a
Anwesh43 <anweshthecool0@gmail.com>
2018-04-01 12:49:33 +0530

    adding HShapeAnimator

--- HShapedComponent.js
+++ HShapedComponent.js
@@ -45,3 +45,22 @@ class HShapeState {
         }
     }
 }
+class HShapeAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start (updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
