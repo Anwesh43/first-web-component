6fe6b168643de58f8921be1cd2036d03002b26f3
Anwesh43 <anweshthecool0@gmail.com>
2018-04-01 13:02:24 +0530

    adding fixes in HShapeComponent

--- HShapedComponent.js
+++ HShapedComponent.js
@@ -2,7 +2,7 @@ const w = window.innerWidth, h = window.innerHeight
 class HShapedComponent extends HTMLElement {
     constructor() {
         super()
-        this.img = document.createElement('canvas')
+        this.img = document.createElement('img')
         const shadow = this.attachShadow({mode : 'open'})
         shadow.appendChild(this.img)
         this.animator = new HShapeAnimator()
@@ -14,6 +14,7 @@ class HShapedComponent extends HTMLElement {
         canvas.height = h
         const context = canvas.getContext('2d')
         context.fillStyle = '#212121'
+        context.fillRect(0, 0, w, h)
         this.hShape.draw(context)
         this.img.src = canvas.toDataURL()
     }
@@ -22,6 +23,7 @@ class HShapedComponent extends HTMLElement {
         this.img.onmousedown = (event) => {
             this.hShape.startUpdating(() => {
                 this.animator.start(() => {
+                    this.render()
                     this.hShape.update(() => {
                         this.animator.stop()
                     })
@@ -39,6 +41,7 @@ class HShapeState {
     }
     update(stopcb) {
         this.scales[this.j] += this.dir * 0.1
+        console.log(this.scales[this.j])
         if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
             this.scales[this.j] = this.prevScale + this.dir
             this.j += this.dir
@@ -84,12 +87,12 @@ class HShape {
         context.strokeStyle = 'white'
         context.strokeWidth = Math.min(w, h) / 60
         context.strokeCap = 'round'
-        const Hw = Math.min(w,h)/20, Hh = Math.min(w, h)/8
+        const Hw = Math.min(w,h)/15, Hh = Math.min(w, h)/5
         context.save()
         context.translate(w/2, h/2)
         context.rotate(Math.PI * this.state.scales[2])
         for(var i = 0; i < 2; i++) {
-            const Hx = -Hw * this.state.scales[1] * (1 - 2 * i), Hy = Hh/2  * (this.state.scales[1])
+            const Hx = -Hw * this.state.scales[1] * (1 - 2 * i), Hy = Hh/2  * (this.state.scales[0])
             context.beginPath()
             context.moveTo(0, 0)
             context.lineTo(Hx, 0)
@@ -108,3 +111,4 @@ class HShape {
         this.state.startUpdating(startcb)
     }
 }
+customElements.define('hshaped-comp', HShapedComponent)
