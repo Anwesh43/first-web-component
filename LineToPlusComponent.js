80b74133ca6fe322112c406a37ff172a10800980
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 19:22:58 +0530

    adding LineToPlusComponent class

---
+++ LineToPlusComponent.js
@@ -0,0 +1,22 @@
+const w = window.innerWidth, h = window.innerHeight, size = Math.min(w, h)/2
+class LineToPlusComponent extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode:'open'})
+        this.img = document.createElement('img')
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, size, size)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
+customElements.define('line-to-plus', LineToPlusComponent)
