35a42f57a817444cea2e0d78e1634a40c6f3d22c
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 19:58:21 +0530

    adding Animator class

--- LineToPlusComponent.js
+++ LineToPlusComponent.js
@@ -48,4 +48,24 @@ class LineToPlusState {
         }
     }
 }
+
+class LineToPlusAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start (updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop () {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
 customElements.define('line-to-plus', LineToPlusComponent)
