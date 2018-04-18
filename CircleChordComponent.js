9c722077559fa54416ce070c7a5cfa447d62f468
Anwesh43 <anweshthecool0@gmail.com>
2018-04-18 22:19:44 +0530

    adding CCAnimator class

--- CircleChordComponent.js
+++ CircleChordComponent.js
@@ -47,3 +47,23 @@ class CCState {
         }
     }
 }
+
+class CCAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
