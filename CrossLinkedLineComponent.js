0512226d9886671197f6204b115c19ded78da31d
Anwesh43 <anweshthecool0@gmail.com>
2018-04-24 06:23:20 +0530

    adding CrossLinkedLineComponent

---
+++ CrossLinkedLineComponent.js
@@ -0,0 +1,22 @@
+const w = window.innerWidth, h = window.innerHeight, size = Math.min(w,h)NUM_LINES = 4
+
+class CrossLinkedLineComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode : 'open'})
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, size, size)
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.render()
+    }
+}
