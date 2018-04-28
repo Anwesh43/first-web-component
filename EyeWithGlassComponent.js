a7613ab3962a0b8dcd175e204e2d53b01a7534bb
Anwesh43 <anweshthecool0@gmail.com>
2018-04-28 11:18:27 +0530

    adding EWGAimator class

--- EyeWithGlassComponent.js
+++ EyeWithGlassComponent.js
@@ -54,4 +54,26 @@ class EWGState {
     }
 }
 
+class EWGAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
+
 customElements.define('eye-with-glass', EyeWithGlassComponent)
