864e6454cb32f6ed8a56f045f0a2bbe576f2c2e4
Anwesh43 <anweshthecool0@gmail.com>
2018-05-01 12:01:59 +0530

    adding TreeCircledButtonComponent class

---
+++ TreeCircledButtonComponent.js
@@ -0,0 +1,26 @@
+const w = window.innerWidth, h = window.innerHeight,r = Math.min(w, h)/10, size = Math.min(w, h)/3
+
+class TreeCircledButtonComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode : 'open'})
+        shadow.appendChild(this.img)
+    }
+
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w
+        canvas.height = h
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#212121'
+        context.fillRect(0, 0, w, h)
+        this.img.src = canvas.toDataURL()
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+}
+
+customElements.define('tree-circle-button', TreeCircledButtonComponent)
